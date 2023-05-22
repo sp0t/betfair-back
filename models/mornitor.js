be6fd00a835ac1fefb6513cfd440c46401e88305
sp0t <chenyin1g992@gmail.com>
2023-05-22 03:07:55 -0600

    initial

--- models/mornitor.js
+++ models/mornitor.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 const MonitorSchema = new mongoose.Schema(
   {
     sport: {type: String, required: true},
+    state: {type: Boolean, default: false},
     sites: [],
   },
   { collection: 'mornitor' }
