89cbd6baaf1dfea22b06ed037c763b3d928d1a73
sp0t <chenyin1g992@gmail.com>
2023-05-22 02:22:33 -0600

    initial

--- cron/placebet.js
+++ cron/placebet.js
@@ -4,27 +4,24 @@ const cron = require('node-cron');
 
 const { placebet } = require('./../lib/placebet');
 const { mornitor } = require('./../models/mornitor');
+const { betrate } = require('./../models/betrate');
 
 const runplacebet = () => {
   cron.schedule("*/4 * * * * *", async() => {
-    var tmp = await mornitor.find({state: '0'});
+    var [mornitors, betrates] = await Promise.all([
+      mornitor.find({state: '0'}),
+      betrate.find({})
+    ]); 
+
     var funcs = []
-    for (var x in tmp) {
-        for (var y in tmp[x].sites[0].competition) {
-            funcs.push(placebet(tmp[x].sites[0].name, tmp[x].sites[0].competition[y], tmp[x].sites[1].name, tmp[x].sites[1].competition[y]));
+    for (var x in mornitors) {
+        for (var y in mornitors[x].sites[0].competition) {
+            funcs.push(placebet(mornitors[x].sites[0].name, mornitors[x].sites[0].competition[y], mornitors[x].sites[1].name, mornitors[x].sites[1].competition[y], betrates));
         }
     }
 
     var rets = await Promise.all(funcs)
     
-    // for (var x in tmp) {
-    //     funcs.push(getDepositMaxGas(chains[x], ids[y]))
-    // }
-    // try {
-    //     setBetState();
-    // } catch (error) {
-    //   console.log('setBetState', error);
-    // }
   });
 }
 module.exports.runplacebet = runplacebet;
