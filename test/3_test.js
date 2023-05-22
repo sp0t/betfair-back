87ff55d0dd80a5666f0e2c9b6b7e80100838f857
sp0t <chenyin1g992@gmail.com>
2023-05-22 06:00:05 -0600

    initial

--- test/3_test.js
+++ test/3_test.js
@@ -1,11 +1,18 @@
-var x = 'erecse';
-
-console.log(x.includes('ec'))
-
-var funcs = []
-  for (var x in chains) {
-    for (var y in ids) {
-      funcs.push(getDepositMaxGas(chains[x], ids[y]))
-    }
+require("dotenv").config();
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_HOST+'/'+process.env.DB_NAME, {useNewUrlParser: true, useUnifiedTopology: true})
+var id = '32361812';
+const { odds } = require('./../models/odds');
+const run = async() => {
+  try {
+    console.log('start')
+    var betdata = await odds.find({eventId: '32361811'});
+    console.log(betdata)
+    // betdata.state = 2;
+    // await betdata.save();
+  } catch (error) {
+   console.error(error);
   }
-  var rets = await Promise.all(funcs)
+}
+
+run()
