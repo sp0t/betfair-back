773eeb16f8e3062fa743c5552e59b6253f6c974f
sp0t <chenyin1g992@gmail.com>
2023-05-25 01:56:11 -0600

    initial

--- models/bet.js
+++ models/bet.js
@@ -2,17 +2,16 @@ var mongoose = require('mongoose');
 
 const BetSchema = new mongoose.Schema(
   {
-    site: { type: String, required: true },
-    sportId: { type: String, required: true },
-    sportName: { type: String },
-    competitionId: { type: String, required: true },
-    competitionName: { type: String, required: true },
-    eventId: { type: String, required: true },
-    home: { type: String, required: true },
-    away: { type: String, required: true },
-    state: { type: String, default: 0 },
-    update: {type: Number},
-    market: []
+    site: {type:String, required:true},
+    betdate: {type:String, required:true},
+    away: {type:String, required:true},
+    home: {type:String, required:true},
+    odds: {type:Number, required:true},
+    stake: {type:Number, required:true},
+    place: {type:String, required:true},
+    market:{type:String, required:true},
+    league: {type:Number, required:true},
+    state: {type:Number, default:0}
   },
   { collection: 'bet' }
 )
