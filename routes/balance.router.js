f6f4799a83ee17171edea69bc1c300108f04dbf7
sp0t <chenyin1g992@gmail.com>
2023-06-07 07:04:35 -0600

    initial

--- routes/balance.router.js
+++ routes/balance.router.js
@@ -1,6 +1,5 @@
 module.exports = app => {
     const balance = require("../controller/balance.controller");
-    console.log('getbalance=====================1')
     app.get("/getBalance", balance.getBalance);
     app.post("/setMaxBalance", balance.setMaxBalance);
     app.post("/resetAvailableBalance", balance.resetAvailableBalance);
