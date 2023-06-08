c01675c6e91fbdc50585d8d3e6f5dfa0cc5d42b4
sp0t <chenyin1g992@gmail.com>
2023-06-08 09:53:09 -0600

    initial

--- models/match.js
+++ models/match.js
@@ -1,6 +1,19 @@
 const { number } = require('mathjs');
 var mongoose = require('mongoose');
 
+const StakeModeSchema = new mongoose.Schema({
+  diffmode: { type: Number, default: 0 },
+  betmode: { type: Number, default: 0 },
+  from: { type: Number, default: 0 },
+  to: { type: Number, default: 0 },
+  stake: { type: Number, default: 0 },
+  max: { type: Number, default: 0 },
+  probability: { type: Number, default: 0 },
+  formula: { type: String, default: 'f = (p * (d - 1) - q) / (d - 1)' },
+  kellybalance: { type: Number, default: 0 },
+  state: { type: Boolean, default: false }
+});
+
 const MatchSchema = new mongoose.Schema(
   {
     monitId: { type: String, required: true },
@@ -12,7 +25,7 @@ const MatchSchema = new mongoose.Schema(
     gamedate: {type: String, default: ''},
     update: {type: String},
     betid: {type: String, default:'0'},
-    stakemode: {},
+    stakemode: StakeModeSchema,
     betfairodd:{},
     ps3838odd:{},
     state: { type: Number, default: 0 }
