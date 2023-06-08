24e5735ea68651665aef03256c30973dd485e88a
sp0t <chenyin1g992@gmail.com>
2023-06-08 11:20:40 -0600

    initial

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const WebSocket = require('ws');
+const { updateodds } = require('./cron/updateodd');
+const { runsetBetState } = require('./cron/setbetstate');
+const { runplacebet } = require('./cron/placebet');
+var express = require('express');
+const sportName = 'ALL';
+const competitionName = 'ALL';
 
 mongoose.connect(process.env.DB_HOST+'/'+process.env.DB_NAME, {useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.set('strictQuery', false);
 var mongoDB = mongoose.connection;
 
-const { updateodds } = require('./cron/updateodd');
-const { runsetBetState } = require('./cron/setbetstate');
-const { runplacebet } = require('./cron/placebet');
-
 mongoDB.once('open', function() {
   console.log('--  MogoDB Connected  --');
-  var express = require('express');
+  const wsServer = new WebSocket.Server({ port: process.env.WEBSOCKET_PORT })
+  
   var app = express();
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -32,18 +36,34 @@ mongoDB.once('open', function() {
   app.use('/', (req, res) => {
     res.send('API is working')
   });
+
+  wsServer.on('connection', (ws) => {
+    console.log('WebSocket connected');
+    
+    // Handle WebSocket events
+    ws.on('message', (message) => {
+      console.log(`Received message: ${message}`);
+    });
+    
+    ws.on('close', () => {
+      console.log('WebSocket disconnected');
+    });
+  });  
   
   console.log('--  Server Started  --')
   updateodds();
   // runsetBetState();
   // runplacebet();
 
-  const port = process.env.PORT || 4200;
+  const port = process.env.SERVER_PORT || 4200;
   //Starting a server
   app.listen(port, () => {
     console.log(`app is running at ${port}`);
   });
 });
 
+module.exports.wsServer = wsServer;
+module.exports.sportName = sportName;
+module.exports.competitionName = competitionName;
 
 
