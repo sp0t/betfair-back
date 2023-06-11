0915a6217a1bbab82cd2f2a6205f74cf2b3b0934
sp0t <chenyin1g992@gmail.com>
2023-06-10 21:09:45 -0600

    init

--- test/4_placebet.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DB_HOST+'/'+process.env.DB_NAME, {useNewUrlParser: true, useUnifiedTopology: true})
-
-const { runplacebet } = require('./../cron/placebet');
-
-runplacebet()
