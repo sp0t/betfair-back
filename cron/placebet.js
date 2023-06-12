088805dc6aceb0f4d4966ae94fd6d5426e13df58
sp0t <chenyin1g992@gmail.com>
2023-06-12 01:04:26 -0600

    init

--- cron/placebet.js
+++ cron/placebet.js
@@ -6,21 +6,19 @@ const { placebet } = require('./../lib/placebet');
 const { monitor } = require('../models/monitor');
 const { stakemode } = require('../models/stakemode');
 const { auth } = require('../models/auth')
-const { balance } = require('../models/balance')
 
 const runplacebet = () => {
   cron.schedule("*/3 * * * * *", async() => {
-    var [monitors, stakemodes, seesion, totalfund] = await Promise.all([
-        monitor.find({betting: true}),
+    var [monitors, stakemodes, seesion] = await Promise.all([
+       monitor.find({betting: true}),
        stakemode.find({}),
-       auth.find({site: 'betfair'}),
-       balance.find({})
+       auth.find({site: 'betfair'})
     ]); 
 
     if ((monitors.length != 0) && (stakemode.length != 0) && (seesion.length != 0)) {
       var funcs = []
       for (var x in monitors) {
-        funcs.push(placebet(monitors[x].monitId, monitors[x].sport, monitors[x].sites[0].competition[0], stakemodes, seesion[0].token, monitors[x].kellymode, totalfund[0]));
+        funcs.push(placebet(monitors[x].monitId, monitors[x].sport, monitors[x].sites[0].competition[0], stakemodes, seesion[0].token, monitors[x].kellymode));
       }
   
       var rets = await Promise.all(funcs)
