5a329257ce56774b26788d87d49e95cbdac3c09a
sp0t <chenyin1g992@gmail.com>
2023-06-15 11:54:54 -0600

    init

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ mongoDB.once('open', function() {
 
   console.log('--  Server Started  --')
   updateodds();
-  runsetBetState();
+  // runsetBetState();
   // runplacebet();
 
   const port = process.env.SERVER_PORT || 4200;
